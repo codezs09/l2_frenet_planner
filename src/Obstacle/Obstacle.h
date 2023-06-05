9df13c5b3e47846afa9aafd5f02de4029d6258f6
Sheng Zhu <shengzhu90@hotmail.com>
2023-06-04 22:46:05 -0700

    change obstacle interface

--- src/Obstacle/Obstacle.h
+++ src/Obstacle/Obstacle.h
@@ -1,33 +1,51 @@
 #ifndef FRENETOPTIMALTRAJECTORY_OBSTACLE_H
 #define FRENETOPTIMALTRAJECTORY_OBSTACLE_H
 
+#include <cmath>
 #include <eigen3/Eigen/Dense>
+#include <map>
 #include <memory>
+#include <pair>
 #include <vector>
 
-#include "utils.h"
+#include "utils/lookup_table_1d.h"
+#include "utils/utils.h"
 
 using namespace Eigen;
 using namespace std;
 
 class Obstacle {
  public:
-  std::pair<Vector2f, Vector2f> bbox;
-  Obstacle(Vector2f first_point, Vector2f second_point,
-           double obstacle_clearance);
+  // std::pair<Vector2f, Vector2f> bbox;
+
+  Obstacle() = delete;
+  Obstacle(Pose pose, double length, double width, double obstacle_clearance)
+      : pose_(pose), obstacle_clearance_(obstacle_clearance) {
+    predict_poses_.push_back({0.0, pose_});
+  }
+  Obstacle(Pose pose, Twist twist, double length, double width,
+           double obstacle_clearance)
+      : Obstacle(pose, length, width, obstacle_clearance), twist_(twist) {}
+
+  void setSpeedLookupTable(const map<double, double> &spd_profile);
+  bool predictPoses(double cur_timestamp, double max_duration, double dt);
+  bool predictPoses(const map<double, double> &spd_profile,
+                    double cur_timestamp, double max_duration, double dt);
+
   bool isSegmentInObstacle(Vector2f &p1, Vector2f &p2);
   bool isPointNearObstacle(Vector2f &p, double radius);
   double getArea();
 
  private:
-  unique_ptr<Pose> pose_frenet_ = nullptr;
-  unique_ptr<Pose> pose_cartesian_ = nullptr;
-  double length_;  // parallel with yaw
-  double width_;
-  vector<double> wx_;
-  vector<double> wy_;
-  vector<Pose> pred_poses_frenet_;  // prediction
-  vector<Pose> pred_poses_cartesian_;
+  const double length_;  // parallel with yaw
+  const double width_;
+  const double obstacle_clearance_;
+
+  Pose pose_;
+  map<double, Pose> predict_poses_;  // map: timestapm -> pose
+  Twist twist_;
+
+  unique_ptr<LookupTable1D> tbl_time_to_speed_ = nullptr;
 };
 
 #endif  // FRENETOPTIMALTRAJECTORY_OBSTACLE_H
