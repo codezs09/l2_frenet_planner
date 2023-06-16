87533839c796aa661e92b0931c4d9ed48ad25260
Sheng Zhu <shengzhu90@hotmail.com>
2023-06-16 16:48:54 -0700

    introduce lane class

---
+++ src/Lane/Lane.cpp
@@ -0,0 +1,61 @@
+#include "Lane.h"
+
+void Lane::SetWaypoints(const WayPoints& wp) { wp_ = wp; }
+
+bool Lane::GetLaneBoundaries(WayPoints* left_boundary,
+                             WayPoints* right_boundary) const {
+  *left_boundary = left_boundary_;
+  *right_boundary = right_boundary_;
+  return true;
+}
+
+void Lane::SetLaneBoundaries(const WayPoints& left_boundary,
+                             const WayPoints& right_boundary) {
+  left_boundary_ = left_boundary;
+  right_boundary_ = right_boundary;
+}
+
+void Lane::SetWayPoints(const WayPoints& wp, double lane_width) {
+  wp_ = wp;
+  lane_width_ = lane_width;
+  CalculateLaneBoundariesFromWaypoints();
+}
+
+bool Lane::GetLeftLane(Lane* left_lane) {
+  if (left_lane_ == nullptr) {
+    return false;
+  }
+  *left_lane = *left_lane_;
+  return true;
+}
+
+bool Lane::GetRightLane(Lane* right_lane) {
+  if (right_lane_ == nullptr) {
+    return false;
+  }
+  *right_lane = *right_lane_;
+  return true;
+}
+
+void Lane::CalculateLaneBoundariesFromWaypoints() {
+  left_boundary_[0].clear();
+  left_boundary_[1].clear();
+  right_boundary_[0].clear();
+  right_boundary_[1].clear();
+  double half_lane_width = lane_width_ / 2.0;
+
+  CubicSpline2D csp(wp_[0], wp_[1]);
+
+  for (int i = 0; i < wp_[0].size(); ++i) {
+    double s = csp.find_s(wp_[0][i], wp_[1][i]);
+    double x = csp.calc_x(s);
+    double y = csp.calc_y(s);
+    double yaw = csp.calc_yaw(s);
+
+    left_boundary_[0].push_back(x - half_lane_width * sin(yaw));
+    left_boundary_[1].push_back(y + half_lane_width * cos(yaw));
+
+    right_boundary_[0].push_back(x + half_lane_width * sin(yaw));
+    right_boundary_[1].push_back(y - half_lane_width * cos(yaw));
+  }
+}
---
+++ src/Lane/Lane.h
@@ -0,0 +1,78 @@
+#ifndef FRENETOPTIMALTRAJECTORY_LANE_H
+#define FRENETOPTIMALTRAJECTORY_LANE_H
+
+#include <cmath>
+#include <vector>
+
+#include "CubicSpline/CubicSpline2D.h"
+#include "utils/utils.h"
+
+using namespace std;
+using namespace utils;
+
+class Lane {
+ public:
+  Lane(const WayPoints& wp, double lane_width = 4.0, Lane* left_lane = nullptr,
+       Lane* right_lane = nullptr)
+      : wp_(wp),
+        lane_width_(lane_width),
+        left_lane_(left_lane),
+        right_lane_(right_lane) {
+    CalculateLaneBoundariesFromWaypoints();
+  }
+  Lane(const WayPoints& wp, double lane_width, Lane* left_lane,
+       Lane* right_lane, const WayPoints& left_boundary,
+       const WayPoints& right_boundary)
+      : wp_(wp),
+        lane_width_(lane_width),
+        left_boundary_(left_boundary),
+        right_boundary_(right_boundary),
+        left_lane_(left_lane),
+        right_lane_(right_lane) {}
+
+  // copy constructor
+  Lane(const Lane& other)
+      : wp_(other.wp_),
+        lane_width_(other.lane_width_),
+        left_boundary_(other.left_boundary_),
+        right_boundary_(other.right_boundary_),
+        left_lane_(other.left_lane_),
+        right_lane_(other.right_lane_) {}
+
+  // copy assignment operator
+  Lane& operator=(const Lane& other) {
+    if (this != &other) {
+      // copy and swap idiom
+      Lane copy(other);
+      std::swap(*this, copy);
+    }
+    return *this;
+  }
+
+  void SetWayPoints(const WayPoints& wp, double lane_width = 4.0);
+
+  bool GetLaneBoundaries(WayPoints* left_boundary,
+                         WayPoints* right_boundary) const;
+  void SetLaneBoundaries(const WayPoints& left_boundary,
+                         const WayPoints& right_boundary);
+
+  const WayPoints& GetWayPoints() const { return wp_; }
+  double GetLaneWidth() const { return lane_width_; }
+  bool GetLeftLane(Lane* left_lane);
+  bool GetRightLane(Lane* right_lane);
+  Lane* MutableLeftLane() { return left_lane_; }    // check nullptr before use
+  Lane* MutableRightLane() { return right_lane_; }  // check nullptr before use
+
+ private:
+  void CalculateLaneBoundariesFromWaypoints();
+
+  WayPoints wp_;
+  WayPoints left_boundary_;
+  WayPoints right_boundary_;
+
+  double lane_width_;
+  Lane* left_lane_ = nullptr;
+  Lane* right_lane_ = nullptr;
+};
+
+#endif  // FRENETOPTIMALTRAJECTORY_LANE_H
